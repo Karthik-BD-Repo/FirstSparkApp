a3f7eaa6341d870bce684a9856877c2133abdee5
karthik <bkarthiksvce@gmail.com>
2019-07-27 18:36:15 +0530

    intial commit

--- src/main/scala/com/spark/FirstSparkApplication.scala
+++ src/main/scala/com/spark/FirstSparkApplication.scala
@@ -10,10 +10,10 @@ object FirstSparkApplication {
     val sc =new SparkContext(conf)
     val input = sc.textFile("D:/Karthik/Common.txt")
 
-    val count1 = input.flatMap(line ⇒ line.split(" "))
+    val count = input.flatMap(line ⇒ line.split(" "))
       .map(word ⇒ (word, 1))
       .reduceByKey(_ + _)
-    count1.saveAsTextFile("D:/Karthik/wordcount/")
+    count.saveAsTextFile("D:/Karthik/wordcount/")
     //count.saveAsTextFile("/users/sairavi/wordcount/")
     System.out.println("OK")
   }
